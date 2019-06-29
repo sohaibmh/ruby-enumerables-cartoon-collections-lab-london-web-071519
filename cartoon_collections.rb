59c3d4dc1724ebf6513ce6ebe5759e521d29c146
Sohaib Mubashir <sohaibmh@yahoo.com>
2019-06-29 11:06:26 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,35 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+ array.each.with_index {|element, index| puts "#{index+1} " + "#{element}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+dwarf_names = ["Doc", "Dopey", "Bashful", "Grumpy"]
+
+roll_call_dwarves(dwarf_names)
+puts
+
+def summon_captain_planet(array)
+  a = []
+  array.each {|element| a.push element.capitalize + "!"}
+  a
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+planeteers = ["earth", "wind", "fire", "water", "heart"]
+
+print summon_captain_planet(planeteers)
+puts
+puts
+long_planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+
+def long_planeteer_calls(array)
+  array.any? {|x| x.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+puts long_planeteer_calls(long_planeteer_calls)
+puts
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find {|cheese| cheese == "cheddar" or cheese == "gouda" or cheese == "camembert"}
 end
+
+cheddar_cheese = ["banana", "cheddar", "sock", "gouda"]
+print find_the_cheese(cheddar_cheese)
